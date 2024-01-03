e793b3b5754924d18a2a590361f3f567ee9024a6
jeremy-muya <115176929+jeremy-muya@users.noreply.github.com>
2024-01-03 17:54:09 +0300

    Update stara.py

--- stara.py
+++ stara.py
@@ -4,9 +4,9 @@ from datetime import datetime
 
 # Dummy user
 users = {
-    'admin': {'password': 'admin123', 'role': 'admin'},
-    'user1': {'password': 'user123', 'role': 'user'},
-    'user2': {'password': 'user123', 'role': 'user'},
+    'Ng'ang'a': {'password': 'paswadnoma2024', 'role': 'admin'},
+    'Grace': {'password': 'Grace2024password', 'role': 'user'},
+    'Mogere': {'password': 'passwordMogere2024.', 'role': 'user'},
 }
 
 # Initialize data storage
